341eb8350eec562ae0b04601a60acca2cbad2ac0
SeveryukhinVeniamin <severyukhinvd@yandex.ru>
2026-03-16 16:51:48 +0300

    Add files via upload

--- Проект/main.py
+++ Проект/main.py
@@ -125,7 +125,36 @@ def choice(planet_name):
 
                     </body>
                 </html>"""
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
 
+                        <title>Колонизация</title>
+                    </head>
+                    <body>
+                        <h1>Results of custing:</h1>
+
+                        <div class="alert alert-primary" role="alert">
+                          Pretendent {nickname}
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                          Your reyting after {level} level
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                          {rating}
+                        </div>
+
+                    </body>
+                </html>"""
 
 if __name__ == '__main__':
     app.run('127.0.0.1', 8080)
